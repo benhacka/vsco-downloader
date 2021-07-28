7c55a51b12a7276ad8889aeaafefb10dfd8fd789
foobar <bender1233zbt@gmail.com>
2021-07-29 02:02:14 +0300

    Added __main__ file w/ entry point.

--- vsco_downloader/__main__.py
+++ vsco_downloader/__main__.py
@@ -1,10 +1,20 @@
+import asyncio
+import logging
+
+from .downloader import VscoGrabber
+
+
 def parse_arg():
     pass
 
 
 def main():
-    pass
+    logging.basicConfig(level=logging.INFO)
+    grabber = VscoGrabber()
+    users = []
+
+    asyncio.run(grabber.parse_users(users))
 
 
 if __name__ == '__main__':
-    pass
+    main()
