712ce8b3e3fdd158f9d72eeade5218673de9693e
foobar <bender1233zbt@gmail.com>
2021-07-29 01:59:06 +0300

    Added the containers for picture/mini-video/video(m3u8) contents.

---
+++ vsco_downloader/container.py
@@ -0,0 +1,124 @@
+import asyncio
+import datetime
+import logging
+import os
+import re
+from pprint import pprint
+
+
+class VscoContent:
+    def __init__(self, content_dict):
+        self._content_dict = content_dict
+
+    @property
+    def download_url(self):
+        raise NotImplementedError
+
+    @property
+    def datetime(self):
+        capture_date = (self._content_dict.get('captureDate')
+                        or self._content_dict.get('capture_date')
+                        or self._content_dict.get('created_date')
+                        or self._content_dict.get('uploadDate')
+                        or self._content_dict.get('last_updated'))
+        if not capture_date:
+            logging.getLogger('Content').warning(
+                'Datetime were not found in %s', str(self._content_dict))
+            raise ValueError
+            return ''
+        return datetime.datetime.fromtimestamp(
+            capture_date / 1000).strftime("%Y-%m-%d_%H-%M-%S_")
+
+    def get_file_name(self, *download_path):
+        if not self.download_url:
+            return None
+        name = self.download_url.split('/')[-1]
+        return os.path.join(*download_path, f'{self.datetime}{name}')
+
+    @classmethod
+    def get_content_type(cls, content_dict) -> str:
+        if 'videoUrl' in content_dict or 'video_url' in content_dict:
+            return 'mini_video'
+        if 'playback_url' in content_dict:
+            return 'video'
+        return 'photo'
+
+
+class VscoPhoto(VscoContent):
+    @property
+    def download_url(self):
+        try:
+            return 'https://' + (self._content_dict.get('responsiveUrl')
+                                 or self._content_dict.get('responsive_url'))
+        except TypeError:
+            pprint(self._content_dict)
+            return None
+
+
+class VscoMiniVideo(VscoContent):
+    @property
+    def download_url(self):
+        return 'https://' + (self._content_dict.get('videoUrl')
+                             or self._content_dict.get('video_url'))
+
+
+class VscoVideo(VscoContent):
+    def __init__(self, content_dict):
+        self._temp_dir = None
+        super().__init__(content_dict)
+
+    def set_temp_dir(self, temp_dir):
+        self._temp_dir = temp_dir
+
+    @property
+    def download_url(self):
+        return self._content_dict['playback_url']
+
+    def get_file_name(self, *download_path):
+        if not self.download_url:
+            return None
+        # TODO: make container as param
+        name = self._content_dict['_id'] + '.mp4'
+        return os.path.join(*download_path, f'{self.datetime}{name}')
+
+    @classmethod
+    def choice_best_resolution(cls, m3u8_text):
+        res_dict = {}
+        splitted_lines = m3u8_text.splitlines()
+        for index, line in enumerate(splitted_lines):
+            ext_string = (re.search(r'RESOLUTION=(\d*)x\d*', line))
+            if not ext_string:
+                continue
+            res_dict[int(ext_string.group(1))] = splitted_lines[index + 1]
+        return res_dict[max(res_dict)]
+
+    def generate_ffmpeg_concat(self, files, out_file):
+        concat_string = self._generate_concat_string(files)
+        ffmpeg_cmd = (f'ffmpeg -hide_banner -loglevel error -y -i '
+                      f'"{concat_string}" -c copy -bsf:a aac_adtstoasc '
+                      f'{out_file}')
+        return ffmpeg_cmd
+
+    @staticmethod
+    async def run_ffmpeg(ffmpeg_cmd):
+        proc = await asyncio.create_subprocess_shell(
+            ffmpeg_cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await proc.communicate()
+        return stderr.decode() if stderr else None
+
+    @classmethod
+    async def is_ffmpeg_exists(cls):
+        # TODO: make ffmpeg bin alias as param
+        return not cls.run_ffmpeg('ffmpeg -version')
+
+    def _generate_concat_string(self, files):
+        return 'concat:' + '|'.join(
+            (os.path.join(self.temp_content_dir, file) for file in files))
+
+    @property
+    def temp_content_dir(self):
+        assert self._temp_dir
+        return os.path.join(self._temp_dir,
+                            self.download_url.split('/')[-1].split('.')[0])
