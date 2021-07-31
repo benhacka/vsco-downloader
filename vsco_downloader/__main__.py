f91130942575ee5996d52dbb7891997324306bc0
foobar <bender1233zbt@gmail.com>
2021-07-31 03:42:44 +0300

    Moved grabber init inside async method runs in asyncio.run Added calling and passing args to the init and the main method of the grabber.

--- vsco_downloader/__main__.py
+++ vsco_downloader/__main__.py
@@ -4,23 +4,22 @@ import logging
 import time
 from typing import List
 
+from vsco_downloader.argparser import parse_arg
 from vsco_downloader.user import VscoUser
 from .downloader import VscoGrabber
 
 
-def parse_arg():
-    pass
-
-
-def main():
-    logging.basicConfig(level=logging.INFO)
-    grabber = VscoGrabber()
-
-    users = []
+async def a_main():
+    try:
+        init_dict, parse_dict = await parse_arg()
+    except ValueError as e:
+        logging.error(e)
+        return
+    grabber = VscoGrabber(**init_dict)
 
     st_time = time.time()
     try:
-        users: List[VscoUser] = asyncio.run(grabber.parse_users(users))
+        users: List[VscoUser] = await grabber.parse_users(**parse_dict)
         stat_logger = logging.getLogger('Stat')
         for user in users:
             log = (stat_logger.warning if user.stat.has_error
@@ -31,6 +30,14 @@ def main():
     finally:
         delta = datetime.timedelta(seconds=time.time() - st_time)
         logging.info('Script finished in %s', delta)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(a_main())
+    except KeyboardInterrupt:
+        logging.info('Finishing...')
 
 
 if __name__ == '__main__':
