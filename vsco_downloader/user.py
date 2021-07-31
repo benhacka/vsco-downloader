b4e13c5139866c952c171546fc6ef6c34d1f9a3e
foobar <bender1233zbt@gmail.com>
2021-07-31 06:42:12 +0300

    hot fix

--- vsco_downloader/user.py
+++ vsco_downloader/user.py
@@ -216,7 +216,7 @@ class VscoUser:
             full_url += '/'
         full_url = full_url.replace('vsco.co:443', 'vsco.co')
         username, *trail = full_url.split('vsco.co/')[-1].split('/')
-        if len(trail) > 1:
+        if len(trail) > 0:
             trail = trail[0]
             is_full = False
         if not username or (trail and trail not in cls.allowed_vsco_path):
