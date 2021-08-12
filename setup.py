f7fd3274152768eaa09245200e68e83d04b2a427
foobar <bender1233zbt@gmail.com>
2021-08-12 03:59:14 +0300

    Changed setup.py for PyPi

--- setup.py
+++ setup.py
@@ -1,22 +1,38 @@
 #!/usr/bin/env python3
-import sys
 import setuptools
 from vsco_downloader import __version__
+from vsco_downloader.__main__ import py_version_checker
 
-if sys.version_info < (3, 7):
-    sys.exit('Python < 3.7 is not supported')
+py_version_checker()
 
 with open('requirements.txt') as f:
     required = [requirement.strip() for requirement in f.readlines()]
 
+GIT_URL = '"https://github.com/benhacka/vsco-downloader'
+
 setuptools.setup(
-    name='vsco_downloader',
+    name='vsco-downloader',
     version=__version__,
-    description='VSCO Downloader',
+    description='VSCO download manager on pure REST',
+    long_description='README.md',
+    long_description_content_type='text/markdown',
     zip_safe=False,
+    author='anon',
+    author_email='anon@fake-mail.foobar',
+    url=GIT_URL,
+    download_url=f'{GIT_URL}/archive/refs/tags/v.{__version__}.tar.gz',
     include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     packages=setuptools.find_packages(),
     install_requires=required,
+    keywords="vsco download scrape parse photo social-network".split(),
     python_requires='>=3.7',
     entry_points={
         'console_scripts': [
